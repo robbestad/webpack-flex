845e24a13655d01fc887e0705e412cfc048bc988
Sven Anders Robbestad <sven@robbestad.com>
2016-12-22 17:00:18 +0100

    Added build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('react-flex-slider/assets/index.css');
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Slider = require('react-flex-slider');
+var container = document.getElementById('container');
 ReactDOM.render(<Slider />, container);
